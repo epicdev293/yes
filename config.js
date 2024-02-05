dd3d539dbbd6a6ad1ed4035db2818945b0692c28
epicdev293 <156992896+epicdev293@users.noreply.github.com>
2024-02-05 01:38:43 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,20 +3,6 @@ const config = {
   users: {
     // username: 'password', you can add multiple users.
     Nuggets: 'ikWmds6x$',
-    fbi: 'DOGS123_',
-    anything: 'lmao',
-    REDACTED: 'RocketShip1',
-    User: 'User',
-    coolbloxplayer1: 'Lovesjana',
-    150004151: '017976',
-    Fraxer: '081253432',
-    Dustin: '12345',
-    stm2392: '32.1344.2214.221',
-    hellfire: '655902009',
-    pico: 'pico',
-    elkyn08: '339941303',
-    Ayan: 'uselicHalNur',
-    Fitzcharles: 'imEsPaLCoRAN',
     Igor: 'Igor123',
   }
 }
